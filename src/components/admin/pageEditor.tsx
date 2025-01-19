9350ef89afdbea67a50f6b209b7d40fdab215253
Skilz101 <ijijohn356@gmail.com>
2025-01-19 03:49:08 +0100

    form changes

--- src/components/admin/pageEditor.tsx
+++ src/components/admin/pageEditor.tsx
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react";
 import TemplateSelector from "./templateSelector";
 import ThemeSelector, { LogoUpload } from "./theme";
 import TextContent from "./textContent";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const formSchema = z.object({
   template: z.nativeEnum(Template),
@@ -14,9 +15,25 @@ const formSchema = z.object({
   logo: z.string(),
   title: z.string(),
   subTitle: z.string(),
+  name: z.boolean(),
+  email: z.boolean(),
+  phoneNumber: z.boolean(),
 });
 
 export default function PageEditor() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      template: undefined,
+      theme: undefined,
+      logo: "",
+      title: "",
+      subTitle: "",
+      name: false,
+      email: false,
+      phoneNumber: false,
+    },
+  });
   const [template, setTemplate] = useState<Template>();
   const [theme, setTheme] = useState<Theme>();
   const [logo, setLogo] = useState<string>("");
@@ -37,6 +54,14 @@ export default function PageEditor() {
     setLogo(selectedLogo);
   };
 
+  const handleTitleChange = (newTitle: string) => {
+    seTitle(newTitle);
+  };
+
+  const handleSubTitlechange = (newSubTitle: string) => {
+    setSubTitle(newSubTitle);
+  };
+
   // Log the updated template
   useEffect(() => {
     if (template) {
@@ -48,7 +73,13 @@ export default function PageEditor() {
     if (logo) {
       console.log(`Updated logo: ${logo}`);
     }
-  }, [template, theme, logo]);
+    if (title) {
+      console.log(`Updated title: ${title}`);
+    }
+    if (subTitle) {
+      console.log(`Updated subTitle: ${subTitle}`);
+    }
+  }, [template, theme, logo, title, subTitle]);
   return (
     <main className="flex flex-col space-y-8">
       <div className="flex flex-col space-y-5">
@@ -66,11 +97,16 @@ export default function PageEditor() {
       <hr className="mt-5" />
       {/* content */}
 
-      <div className="flex flex-col space-y-5">
+      <div className="flex flex-col space-y-5 mb-5">
         <h3 className="text-3xl">Content</h3>
 
         <div className="flex flex-col space-y-3">
-          <TextContent />
+          <TextContent
+            title={title}
+            subTitle={subTitle}
+            handleTitleChange={handleTitleChange}
+            handleSubTitlechange={handleSubTitlechange}
+          />
         </div>
       </div>
     </main>
--- src/components/admin/textContent.tsx
+++ src/components/admin/textContent.tsx
@@ -1,17 +1,102 @@
 "use client";
 
+import { useState } from "react";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
+import { Textarea } from "../ui/textarea";
 
-export default function TextContent() {
+interface TextInterface {
+  title: string;
+  subTitle: string;
+  handleTitleChange: (newTitle: string) => void;
+  handleSubTitlechange: (newSubTitle: string) => void;
+}
+export default function TextContent({
+  title,
+  subTitle,
+  handleTitleChange,
+  handleSubTitlechange,
+}: TextInterface) {
+  const [modifiedtitle, setTitle] = useState<string>(title);
+  const [modifiedsubtitle, setSubTitle] = useState<string>(subTitle);
+
+  const onTitleChange = (selectedTitle: string) => {
+    handleTitleChange(selectedTitle);
+    setTitle(selectedTitle);
+  };
+
+  const onSubTitleChange = (selectedTitle: string) => {
+    handleSubTitlechange(selectedTitle);
+    setSubTitle(selectedTitle);
+  };
   return (
     <div className="flex flex-col space-y-3">
       <div className="flex space-y-2 flex-col">
-        <Label htmlFor="title">Title Text:</Label>
-        <Input
-          id="title"
-          placeholder="this will be the h1 tag of your landing page"
-        />
+        <Label htmlFor="title" className="text-xl">
+          Title Text:
+        </Label>
+        <div className="flex flex-col space-y-1">
+          <div className="flex flex-col space-y-2 items-end justify-end">
+            <Input
+              id="title"
+              placeholder="this will be the h1 tag of your landing page"
+              onChangeCapture={(e) =>
+                onTitleChange((e.target as HTMLInputElement).value)
+              }
+            />
+            <div className="flex items-center space-x-2">
+              <span
+                className={`${
+                  title?.length > 60 ? "text-red-600" : "text-muted-foreground"
+                }`}
+              >
+                {title?.length}
+              </span>
+              /60
+            </div>
+          </div>
+          {title?.length > 60 && (
+            <p className="text-red-600">
+              You can only write a maximum of 60 characters for your title
+            </p>
+          )}
+        </div>
+      </div>
+
+      <div className="flex space-y-2 flex-col">
+        <Label htmlFor="sub-title" className="text-xl">
+          Sub Title Text:
+        </Label>
+
+        <div className="flex flex-col space-y-1">
+          <div className="flex flex-col space-y-2 items-end justify-end">
+            <Textarea
+              id="sub-title"
+              placeholder="this will be the h1 tag of your landing page"
+              onChangeCapture={(e) =>
+                onSubTitleChange((e.target as HTMLInputElement).value)
+              }
+            />
+
+            <div className="flex items-center space-x-2">
+              <span
+                className={`${
+                  subTitle?.length > 160
+                    ? "text-red-600"
+                    : "text-muted-foreground"
+                }`}
+              >
+                {subTitle?.length}
+              </span>
+              /160
+            </div>
+          </div>
+          {subTitle?.length > 60 && (
+            <p className="text-red-600">
+              You can only write a maximum of 160 characters for your sub title
+            </p>
+          )}
+        </div>
       </div>
     </div>
   );
